3399fc04956e361897728209c183629e20876fd6
Danil <danil130999@gmail.com>
2019-04-15 17:43:56 +0300

    reworked form to handle incorrect inputs

--- form.py
+++ form.py
@@ -4,8 +4,8 @@ from wtforms import StringField, IntegerField, SubmitField
 
 class SearchForm(FlaskForm):
     region = StringField('Регион', validators=[])
-    rooms = StringField('Количество комнат', validators=[])
-    price = StringField('Цена', validators=[])
+    rooms = IntegerField('Количество комнат', validators=[])
+    price = IntegerField('Цена', validators=[])
     descr = StringField('Описание', validators=[])
     submit = SubmitField('Поиск', validators=[])
 
