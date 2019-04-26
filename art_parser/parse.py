a619919cff90f5b1403d917affbe42fa95e8b21e
Danil <danil130999@gmail.com>
2019-04-26 18:38:57 +0300

    price now separated into uah + usd, price, rooms are now integers

--- art_parser/parse.py
+++ art_parser/parse.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as BS
+import re
 
 
 class DRIAParser:
-    cardViewBaseUrl = "https://dom.ria.com"
+    card_view_base_url = "https://dom.ria.com"
 
     def __init__(self, last_art_ref="", amt=100):
         """
@@ -20,39 +21,42 @@ class DRIAParser:
     def set_last_ref(self, ref):
         self.last_art = ref
 
-    def _get_title(self, tag):  # region is defined in card title isn't it redundant to search for region separately?
+    def _get_title(self, tag):
         """Extract title (link, text) from card"""
-        aTag = tag.find('div', class_='mb-10').a
-        if aTag:
-            link = aTag['href']
-            data = aTag.text.strip()
-            metroTag = tag.find('span', class_="i-block")
-            if metroTag:
-                data += f" {metroTag.text} "
+        a_tag = tag.find('div', class_='mb-10').a
+        if a_tag:
+            link = a_tag['href']
+            data = a_tag.text.strip()
+            metro_tag = tag.find('span', class_="i-block")
+            if metro_tag:
+                data += f" м. {metro_tag.text} "
             return link, data
 
     def _get_description(self, tag):
         """Extract description from card"""
-        pTag = tag.find('p', class_='descriptions-ticket')
-        if pTag:
-            descr = pTag.span.text.strip()
-            return descr
+        p_tag = tag.find('p', class_='descriptions-ticket')
+        if p_tag:
+            description = p_tag.span.text.strip()
+            return description
 
     def _get_rooms(self, tag):
         """Extract amount of rooms from cards"""
-        listTag = tag.find('ul')
-        if listTag:
-            rooms = listTag.li.text.strip()
-            return rooms
+        list_tag = tag.find('ul')
+        if list_tag:
+            rooms_string = list_tag.li.text.strip()
+            patt = r'(\d+)'
+            rooms_amt = re.search(patt, rooms_string).group(0)
+            return int(rooms_amt)
 
     def _get_price(self, tag):
         """Extract price from tag"""
-        moneyTag = tag.find('div', class_="mb-5 mt-10 pr")
-        if moneyTag:
-            usdVal, uahVal = moneyTag.span.text.strip().split(' / ')
-            usdVal = ''.join(usdVal.split())    # 9 000 -> 9000 to be able to cast it to integer safely in sql query
-            uahVal = ''.join(uahVal.split())
-            return usdVal, uahVal
+        money_tag = tag.find('div', class_="mb-5 mt-10 pr")
+        if money_tag:
+            data = money_tag.text.replace(' ', '').replace('грн', '').replace('$', '')
+            patt = r'(\d+)/(\d+)'
+            match_res = re.search(patt, data)
+            uah_val, usd_val = match_res.group(1), match_res.group(2)
+            return int(usd_val), int(uah_val)
 
     def parse(self):
         next = "/arenda-kvartir/kiev/?page=1"
@@ -68,30 +72,30 @@ class DRIAParser:
 
         :param rel_url: relative to base url
         """
-        resp = requests.get(self.cardViewBaseUrl + rel_url)
+        resp = requests.get(self.card_view_base_url + rel_url)
         if resp.status_code == 200:
             soup = BS(resp.text, 'html.parser')
-            divTag = soup.find("div", id="catalogResults")
-            sections = divTag.find_all("section")
+            div_tag = soup.find("div", id="catalogResults")
+            sections = div_tag.find_all("section")
             pagination = soup.find('div', id='pagination')
-            nextUrl = pagination.find('span', class_='page-item next text-r').a['href'].strip()
+            next_url = pagination.find('span', class_='page-item next text-r').a['href'].strip()
 
             for s in sections:
-                cardLink, cardTitle = self._get_title(s)
-                if self.cardViewBaseUrl + cardLink == self.last_art:           # last parsed article check
+                card_link, card_title = self._get_title(s)
+                if self.card_view_base_url + card_link == self.last_art:           # last parsed article check
                     return ""
                 desc = self._get_description(s)
                 rooms = self._get_rooms(s)
-                price = self._get_price(s)
+                price_usd, price_uah = self._get_price(s)
                 self.cards.append(
-                    (cardTitle, self.cardViewBaseUrl + cardLink, desc, rooms, price)
+                    (card_title, self.card_view_base_url + card_link, desc, rooms, price_usd, price_uah)
                 )
-            return nextUrl
+            return next_url
         else:
             raise Exception # TODO: refactor for project-specific exceptions
 
 
-# if __name__ == '__main__':
-#     from pprint import pprint
-#     p = DRIAParser(amt=100)
-#     pprint(p.parse())
+if __name__ == '__main__':
+    from pprint import pprint
+    p = DRIAParser(amt=10)
+    print(p.parse())
