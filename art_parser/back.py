b0d942da74ceeb63bd3daf695cb31aa1121b4ea2
Danil <danil130999@gmail.com>
2019-04-26 18:41:31 +0300

    added DRIAParser heir class skeleton for parsed articles maintaining purposes

--- art_parser/back.py
+++ art_parser/back.py
@@ -8,7 +8,7 @@ def poll_update(path, delay=3600):
     db = dbmngr.DBManager(path)
     last_ref = db.get_last_ref()
     db.conn_close()
-    parser = parse.DRIAParser(last_art_ref=last_ref, amt=1000)
+    parser = parse.DRIAParser(last_art_ref=last_ref, amt=100)
     while True:
         print("parser awake...")
         db.conn_open(path)
@@ -19,3 +19,33 @@ def poll_update(path, delay=3600):
         db.conn_close()
         print(f"parser asleep for {delay/60} min...")
         time.sleep(delay)
+
+
+class ExtendedParser(parse.DRIAParser):
+
+    def __init__(self, last_art_ref="", amt=100):
+        super().__init__(last_art_ref, amt)
+
+    def check_updated_articles(self, offset: int, n: int):
+        """
+        Checks if any of parsed articles was updated on original website,
+        if so - collects updated articles
+
+
+        :param offset:
+        :param n:
+        :return:
+        """
+        pass
+
+    def check_deleted_articles(self, offset: int, n: int):
+        """
+        Checks if any of parsed articles was deleted on original website,
+        if so - collects them and retrieves
+
+        :param n: amount of last articles to be checked
+        :param offset: offset from start (from latest parsed article)
+        :return: list - deleted articles
+        """
+        pass
+
