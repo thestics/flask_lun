18885f3981a2290da1b8f4ebd38198aad1549898
Danil <danil130999@gmail.com>
2019-05-10 01:13:22 +0300

    all constans and default parameters removed here for clarity

--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
 
-TAIL_TEMPLATE = "&reg={}&price={}&rooms={}&desc={}"
+TAIL_TEMPLATE = "&reg={}&price_min={}&price_max={}&rooms={}&desc={}"
 PATH_TO_DB = 'art_parser/articles.db'
 POLLING_DELAY = 900
+aliases = ['region', 'rooms', 'price_min', 'price_max', 'description']
+form_default_values = {'description': '', 'rooms': 0, 'price_min': 0,
+                       'price_max': 10_000_000}
