b285127a33272aafe96758f6ada79bfa4c92fa7a
Danil <danil130999@gmail.com>
2019-05-10 01:17:16 +0300

    parse.py refactored and added project-specific exception

--- art_parser/parse.py
+++ art_parser/parse.py
@@ -3,16 +3,25 @@ from bs4 import BeautifulSoup as BS
 import re
 
 
+class ParserBaseException(Exception):
+    pass
+
+
+class IncorrectStatusCodeError(ParserBaseException):
+    pass
+
+
 class DRIAParser:
     card_view_base_url = "https://dom.ria.com"
 
     def __init__(self, last_art_ref="", amt=100):
         """
 
-        :param last_art_ref: the newest parsed article. No parsing will be made after this one occurred while
-                      crawling
+        :param last_art_ref: the newest parsed article.
+                             No parsing will be made after this one
+                             occurred while crawling
         :param amt: amount of articles to be parsed
-        If both parameters provided (something) is chosen
+        If both parameters provided first is prioritized
         """
         self.amt = amt
         self.last_art = last_art_ref
@@ -52,7 +61,8 @@ class DRIAParser:
         """Extract price from tag"""
         money_tag = tag.find('div', class_="mb-5 mt-10 pr")
         if money_tag:
-            data = money_tag.text.replace(' ', '').replace('грн', '').replace('$', '')
+            data = money_tag.text.replace(' ', '').replace('грн', '').\
+                    replace('$', '')
             patt = r'(\d+)/(\d+)'
             match_res = re.search(patt, data)
             uah_val, usd_val = match_res.group(1), match_res.group(2)
@@ -78,24 +88,22 @@ class DRIAParser:
             div_tag = soup.find("div", id="catalogResults")
             sections = div_tag.find_all("section")
             pagination = soup.find('div', id='pagination')
-            next_url = pagination.find('span', class_='page-item next text-r').a['href'].strip()
+            next_url_button = pagination.find('span',
+                                              class_='page-item next text-r')
+            next_url = next_url_button.a['href'].strip()
 
             for s in sections:
                 card_link, card_title = self._get_title(s)
-                if self.card_view_base_url + card_link == self.last_art:           # last parsed article check
+                # last parsed article check
+                if self.card_view_base_url + card_link == self.last_art:
                     return ""
                 desc = self._get_description(s)
                 rooms = self._get_rooms(s)
                 price_usd, price_uah = self._get_price(s)
                 self.cards.append(
-                    (card_title, self.card_view_base_url + card_link, desc, rooms, price_usd, price_uah)
+                    (card_title, self.card_view_base_url + card_link,
+                     desc, rooms, price_usd, price_uah)
                 )
             return next_url
         else:
-            raise Exception # TODO: refactor for project-specific exceptions
-
-
-if __name__ == '__main__':
-    from pprint import pprint
-    p = DRIAParser(amt=10)
-    print(p.parse())
+            raise IncorrectStatusCodeError(str(resp))
