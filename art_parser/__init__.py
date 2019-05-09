28e47d8b4552a5e73f7b05e1c4dd068d0f6bb647
Danil <danil130999@gmail.com>
2019-05-09 20:43:05 +0300

    ExtendedParser class added to __all__ variable

--- art_parser/__init__.py
+++ art_parser/__init__.py
@@ -4,7 +4,7 @@ To resolve naming conflicts 'parser' package renamed to 'art_parser'
 
 from .dbmngr import DBManager
 from .parse import DRIAParser
-from .back import poll_update
+from .back import poll_update, ExtendedParser
 
 
-__all__ = ['DBManager', 'DRIAParser', 'poll_update']
+__all__ = ['DBManager', 'DRIAParser', 'poll_update', 'ExtendedParser']
